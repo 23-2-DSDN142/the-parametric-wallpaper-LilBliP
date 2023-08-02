1419e4ff2635e90374fba9631ae320fc429aea70
LilBliP <140349870+LilBliP@users.noreply.github.com>
2023-08-03 09:45:22 +1200

    Update my_wallpaper.js

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,9 +4,10 @@ let burntUmber = "#88362B";
 let olive = "#88742B";
 let garnet = "#742E25";
 let platinum = "#DFE0E0"
-let davyGrey ="#4E555F"
-let gunmetal = "#32373E";
+let charcoal ="#3C434C"
+let gunmetal = "#272C36";
 let richblack = "#171E27";
+let safetyorange = "#EE7F30";
 
 //body parameters
 let pukekoposx = 100;
@@ -31,17 +32,20 @@ let lowerneckthickness = lowernecklength/2;
 
 //pukeko beak parameters
 
-let beakroundness = .3; //should be <1 for best results
-let beakstartx = headposx + 5;
-let beakstarty = headposy + 1;
-let beaktipx = headposx + 10;
-let beaktipy = headposy + 14;
+let beakroundness = .5; //should be <1 for best results
+let beakstartx = headposx + headWidth/2;
+let beakstarty = headposy + headHeight/6;
+let beaktipx = headposx + 8;
+let beaktipy = headposy + 12;
 let beakendx = headposx;
 let beakendy = headposy + headHeight/2;
 let beaktoproundx = (beakstartx + beaktipx) / 2 + beakroundness;
 let beaktoproundy = (beakstarty + beaktipy) / 2;
 let beakbackroundx = (beakstartx + beakendx) / 2;
 let beakbackroundy = (beakstarty + beakendy) / 2 - beakroundness;
+//pond
+let Pondx = 100
+let Pondy = 140
 
 
 function setup_wallpaper(pWallpaper) {
@@ -61,6 +65,29 @@ function wallpaper_background() {
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
+  //pond make look like in pond
+  fill(lapis)
+stroke(lapis)
+strokeWeight(0)
+arc(Pondx, Pondy, 120, 30, 180, 360)
+
+//legs
+stroke(safetyorange);
+strokeWeight(4);
+line(100, 120, 98, 130);//front
+line(95, 110, 80, 120);//back
+
+
+strokeWeight(3.5);
+line(98, 130, 105, 145);//front
+line(80, 120, 78, 140);//back
+
+
+//pond
+fill(lapis)
+stroke(lapis)
+strokeWeight(0)
+arc(Pondx, Pondy, 120, 40, 0, 180)
 
 //neck
 stroke(richblack);
@@ -92,8 +119,14 @@ vertex(beakbackroundx, beakbackroundy);
 endShape(CLOSE);
 
 strokeWeight(2)
-line(148.5, 100.5, 151, 107)
+line(headposx+headWidth/2.5, 100.5, 151, beakstarty)
 
+//eye
+strokeWeight(1.5);
+ellipse(headposx+headWidth/6, headposy-headHeight/4, .5,.75);
+fill(richblack);
+noStroke();
+ellipse(headposx+headWidth/6, headposy-headHeight/4, 1,1);
 
 //opposite side body wing
 fill(richblack);
@@ -110,14 +143,14 @@ arc(95, 101, 80, 8, 130, 300)
   arc(pukekoposx, pukekoposy, pukekoThickness, pukekoHeight, 340, 140);//lapis overbody
 
   fill(gunmetal)
-  arc(pukekoposx+(pukekoThickness/3), pukekoposy, pukekoThickness-(2/3*pukekoThickness), pukekoHeight-20, 260, 325);//dark triangle
+  arc(pukekoposx+(pukekoThickness/3), pukekoposy, pukekoThickness-(2/3*pukekoThickness), pukekoHeight-18, 260, 325);//dark triangle
 
 
 
 
-  //wings
+
   
-
+//white tail feather
 stroke(platinum)
 fill(platinum)
  beginShape();
@@ -128,24 +161,25 @@ endShape(CLOSE);
 
 
 
-  stroke(davyGrey);
-  fill(davyGrey);
+  stroke(charcoal);
+  fill(charcoal);
   beginShape();
   vertex(100, 90);
   vertex(90, 78.8);
   vertex(50, 90);
   endShape(CLOSE);
 
- 
+ //make back dark
   fill(gunmetal);
 strokeWeight(1)
 stroke(gunmetal)
-arc(100, 100, 60, 40, 170, 300)
-
+arc(100, 100, 60, 43, 170, 300)
+//lower back
+arc(90, 105, 30, 30, 90, 180)
+//lowertail feather
 fill(richblack);
 stroke(richblack);
 strokeWeight(0)
 arc(90, 105, 70, 8, 130, 300)
-
 
 }
